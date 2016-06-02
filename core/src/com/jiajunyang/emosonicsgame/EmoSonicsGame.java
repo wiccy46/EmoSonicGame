e00c5f0dac530e841cd3ee91103d2a1614a31547
Jiajun Yang <thejyang@gmail.com>
2016-06-02 12:46:30 +0900

    try shiftarray()

--- core/src/com/jiajunyang/emosonicsgame/EmoSonicsGame.java
+++ core/src/com/jiajunyang/emosonicsgame/EmoSonicsGame.java
@@ -19,12 +19,20 @@ public class EmoSonicsGame extends ApplicationAdapter {
 	int scWidth, scHeight;
 	private SpriteBatch batch; // This is in the render.
 	private OrthographicCamera camera;
-	private Sprite sprite; // Add chacrecteristic to the texture.
+	private Sprite catpawSprite; // Add chacrecteristic to the texture.
 
 	// For the welcome page
 	private Texture welcomePage;
-	Texture mario;
-	Texture catpaw;
+	private Texture welcomeWord;
+	private Texture mario;
+	private Texture catpaw;
+
+	int numPaws = 5;
+	// It takes x, y, alpha
+	float[]catpawAlpha = new float[numPaws];
+	float[] catpawX = new float[numPaws];
+	float[] catpawY = new float[numPaws];
+
 
 	Vector2 position;
 
@@ -40,6 +48,7 @@ public class EmoSonicsGame extends ApplicationAdapter {
 	TextButton button;
 	Skin skin;
 	BitmapFont font;
+	int catIdx = 0;
 
 	// InputProcessor is a userful thing if you want full access.
 
@@ -49,13 +58,15 @@ public class EmoSonicsGame extends ApplicationAdapter {
 		scWidth = Gdx.graphics.getWidth();
 		scHeight = Gdx.graphics.getHeight();
 		welcomePage = new Texture("welcomePage.png");
+		welcomeWord = new Texture("welcomeWord.png");
 		mario = new Texture("mario.png");
 		catpaw = new Texture("touch/catpaw.png");
+		catpawSprite = new Sprite(catpaw);
 		position = new Vector2(50, 50);
 
-//		welcomeWord = new Texture("welcomeWord.png");
+
 //		welcomeWord = new Texture("playCanvas.png");
-//		gameState = 0;
+		gameState = 0;
 
 		font = new BitmapFont();
 		font.setColor(Color.WHITE);
@@ -71,6 +82,19 @@ public class EmoSonicsGame extends ApplicationAdapter {
 		buttonStyle.font = font;
 		button = new TextButton("start", buttonStyle);
 
+		// Init catpaw pos
+		float temp = 1.f;
+		for (int i = 0 ; i < numPaws; i++){
+			catpawAlpha[i] = temp ;
+			temp -= 0.2f;
+			for(int j = 0; j < 2; j ++){
+				catpawX[i] = -50f;
+				catpawY[i] = -50f;
+			}
+
+		}
+		// -------------------
+
 	}
 
 	@Override
@@ -79,15 +103,46 @@ public class EmoSonicsGame extends ApplicationAdapter {
 //		texture.dispose();
 	}
 
+	// Move element one up for the fading effect.
+	public void shiftArray(int x, int y ){
+		for (int i = numPaws - 1 ; i > 0; i--){
+			Gdx.app.log("Debug: ", Integer.toString(i));
+			catpawX[i] = catpawX[i - 1];
+			catpawY[i] = catpawY[i - 1];
+		}
+	}
+
+
 	@Override
 	public void render (){
 		batch.begin();
 		batch.draw(welcomePage, 0, 0, scWidth, scHeight );
 		batch.draw(mario, position.x, position.y);
 
-		if(Gdx.input.justTouched()){
-//			Gdx.app.log("Debug: ", "Pressed");
-			batch.draw(catpaw, Gdx.input.getX() - catpaw.getWidth()/2 ,  Gdx.graphics.getHeight() - Gdx.input.getY()- catpaw.getHeight()/2 );
+		if (gameState == 0){
+			batch.draw(welcomePage, 0, 0, scWidth, scHeight );
+			batch.draw(welcomeWord, scWidth/2 - welcomeWord.getWidth()/2, scHeight - 200 );
+			// Touch to start the game.
+			if (Gdx.input.justTouched()){
+				gameState = 1;
+			}
+
+		}
+		else if (gameState == 1) {
+			if (Gdx.input.justTouched()) {
+				shiftArray(Gdx.input.getX(), Gdx.input.getY());
+
+				catpawX[0] = Gdx.input.getX() - catpaw.getWidth() / 2;
+				catpawY[0] = Gdx.graphics.getHeight() - Gdx.input.getY() - catpaw.getHeight() / 2;
+				catIdx++;
+				if (catIdx == numPaws) {
+					catIdx = 0;
+				}
+			}
+			for (int i = 0; i < numPaws; i++) {
+				catpawSprite.setPosition(catpawX[i], catpawY[i]);
+				catpawSprite.draw(batch, catpawAlpha[i]);
+			}
 		}
 
 		batch.end();
